de1154c63fa135fa1d9e251b25b663144c2f0abd
Kang <kang@Kangs-MacBook-Pro.local>
2020-08-25 00:45:45 +0700

    1st commit

---
+++ tempData.js
@@ -0,0 +1,75 @@
+export default tempData =[
+    {
+        name: "Plan a Trip",
+        color : "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false,   
+            },
+            {
+                title: "Passport Check",
+                completed: true,   
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true,   
+            },
+            {
+                title: "Pack Luggage",
+                completed: false,   
+            }
+       
+        ]
+
+    },
+    {
+        name: "Mt.Catlin",
+        color : "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false,   
+            },
+            {
+                title: "Passport Check",
+                completed: true,   
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true,   
+            },
+            {
+                title: "Pack Luggage",
+                completed: false,   
+            }
+       
+        ]
+
+    },
+    {
+        name: "KangShine",
+        color : "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false,   
+            },
+            {
+                title: "Passport Check",
+                completed: true,   
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true,   
+            },
+            {
+                title: "Pack Luggage",
+                completed: false,   
+            }
+       
+        ]
+
+    },
+  
+];
